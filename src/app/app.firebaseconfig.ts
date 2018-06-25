4304b4551ebc3754cfe590140872881e8b8e1cab
cgsuarez <csuarez@capsule-soft.com>
2018-06-25 16:19:56 -0500

    Eliminacion de firebaseconfig

--- src/app/app.firebaseconfig.ts
+++ src/app/app.firebaseconfig.ts
@@ -1,8 +1,2 @@
 export var config = {
-   apiKey: "AIzaSyAs6UQDYPpGvmSPcoyLMnDThzb5b7nKX_o",
-   authDomain: "mylibrary-fd48c.firebaseapp.com",
-   databaseURL: "https://mylibrary-fd48c.firebaseio.com",
-   projectId: "mylibrary-fd48c",
-   storageBucket: "mylibrary-fd48c.appspot.com",
-   messagingSenderId: "652649285803"
 }
